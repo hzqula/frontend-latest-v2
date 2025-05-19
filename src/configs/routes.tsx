ed09d2dee11fd4051318e03fc2f1b113c5585f79
hzqula <ilooqstrasi@gmail.com>
2025-05-19 09:18:12 +0700

    🎓 add route path for pengumuman

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -15,6 +15,7 @@ import AssessSeminarProposal from "@/components/AssessSeminar";
 import Landing from "@/pages/landing/Landing";
 import SecurityLog from "@/pages/coordinator/SecurityLog";
 import ResetPassword from "@/pages/auth/ResetPassword";
+import Announcement from "@/pages/coordinator/Announcement";
 // import SeminarProposal from "../pages/redirect/SeminarProposal";
 // import AssessSeminarProposal from "../pages/lecturer/seminar-proposal/AssessSeminar";
 
@@ -27,7 +28,6 @@ export const publicRoutes: RouteObject[] = [
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
   { path: "/reset-password", element: <ResetPassword /> },
-  
 ];
 
 export const privateRoutes: RouteObject[] = [
@@ -50,5 +50,9 @@ export const privateRoutes: RouteObject[] = [
   {
     path: "/seminar-proposal/assess/:seminarId",
     element: <AssessSeminarProposal />,
+  },
+  {
+    path: "/pwngumuman",
+    element: <Announcement />,
   },
 ];

a3b1cef94dd19aa9f9d7d25d1be76cf975af63c1
hzqula <ilooqstrasi@gmail.com>
2025-06-08 10:11:52 +0700

    🎓 add some variants on alert comp

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,23 +1,28 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const alertVariants = cva(
-  "relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-3 gap-y-0.5 items-start [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",
+  "relative w-full rounded-lg border px-4 py-3 text-sm grid has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] grid-cols-[0_1fr] has-[>svg]:gap-x-2.5 gap-y-0.5 items-start [&>svg]:size-4.5 [&>svg]:translate-y-1 [&>svg]:text-current",
   {
     variants: {
       variant: {
         default: "bg-card text-card-foreground",
+        info: "text-jewel-blue bg-pastel-blue/50 [&>svg]:text-current *:data-[slot=alert-description]:text-jewel-blue/90 border border-jewel-blue",
+        warning:
+          "text-jewel-yellow bg-pastel-yellow/50 [&>svg]:text-current *:data-[slot=alert-description]:text-jewel-yellow/90 border border-jewel-yellow",
+        success:
+          "text-jewel-green bg-pastel-green/50 [&>svg]:text-current *:data-[slot=alert-description]:text-jewel-green/90 border border-jewel-green",
         destructive:
-          "text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive/90",
+          "text-jewel-red bg-pastel-red/50 [&>svg]:text-current *:data-[slot=alert-description]:text-jewel-red/90 border border-jewel-red",
       },
     },
     defaultVariants: {
       variant: "default",
     },
   }
-)
+);
 
 function Alert({
   className,
@@ -31,7 +36,7 @@ function Alert({
       className={cn(alertVariants({ variant }), className)}
       {...props}
     />
-  )
+  );
 }
 
 function AlertTitle({ className, ...props }: React.ComponentProps<"div">) {
@@ -39,12 +44,12 @@ function AlertTitle({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="alert-title"
       className={cn(
-        "col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight",
+        "col-start-2 line-clamp-1 -mb-0.5 min-h-4 font-bold tracking-tight",
         className
       )}
       {...props}
     />
-  )
+  );
 }
 
 function AlertDescription({
@@ -60,7 +65,7 @@ function AlertDescription({
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Alert, AlertTitle, AlertDescription }
+export { Alert, AlertTitle, AlertDescription };
